2b053306c367956bc13823adde388a5d98789053
kyazim44 <157311933+kyazim44@users.noreply.github.com>
2024-06-02 00:12:16 +0200

    Update right_angled_triangle.py

--- right_angled_triangle.py
+++ right_angled_triangle.py
@@ -4,8 +4,3 @@ for i in range(1, number +1):
     for j in  range(0, i):
         print('*', end='')
     print()
-
-for i in range(-1, 0, -1):
-    for j in range(0, i):
-        print('*', end='')
-    print()
