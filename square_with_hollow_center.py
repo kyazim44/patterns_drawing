d1dc5912ebb9601b534e65a9d43f7ebd4bad3381
kyazim44 <157311933+kyazim44@users.noreply.github.com>
2024-06-07 22:09:21 +0200

    Update square_with_hollow_center.py

--- square_with_hollow_center.py
+++ square_with_hollow_center.py
@@ -5,5 +5,5 @@ for i in range(number):
         if i == 0 or j == 0 or i == number - 1 or j == number - 1:
             print('*', end='')
         else:
-            print('', '', end='')
+            print(' ', end='')
     print()
